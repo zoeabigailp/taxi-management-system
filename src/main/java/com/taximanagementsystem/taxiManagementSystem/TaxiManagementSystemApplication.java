0cd7a26f5396d555ae38752223f03a3905c4be3a
zoeabigailp <zoep5473@gmail.com>
2024-09-23 18:52:32 -0300

    v1

---
+++ src/main/java/com/taximanagementsystem/taxiManagementSystem/TaxiManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.taximanagementsystem.taxiManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taximanagementsystem/taxiManagementSystem/TaxiManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taximanagementsystem.taxiManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
